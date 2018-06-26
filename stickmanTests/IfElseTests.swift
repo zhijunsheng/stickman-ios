73619abfff48db771a59353786bc7443bdc707a8
盛于泰 <shengyutai@mbp2009.local>
2018-06-26 18:34:29 +0800

    created IfElseTests.swift

---
+++ stickmanTests/IfElseTests.swift
@@ -0,0 +1,27 @@
+//
+//  IfElseTests.swift
+//  stickman
+//
+//  Created by 盛于泰 on 18/6/26.
+//  Copyright (c) 2018年 Gold Thumb Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class IfElseTests: XCTestCase {
+
+
+    func testExample() {
+        let a = 5
+        var b = 2
+        let c = a - b
+        b = 10
+        
+        println(a)
+        println(b + a)
+        println(c)
+
+    }
+
+}
