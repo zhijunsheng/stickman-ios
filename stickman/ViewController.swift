06394823be5895d736ba270ba39e3a43fd903c1f
chandler <chandler@chandlerdeMBP.lan>
2018-07-26 11:04:13 +0800

    changing color implemented

--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBOutlet weak var blockView: UIView!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        blockView.backgroundColor = UIColor.black
+    
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func vhgj(_ sender: Any) {
+        blockView.backgroundColor = UIColor.blue
+        
     }
-
-
+    
+    
 }
 
--- stickmanTests/stickmanTests.swift
+++ stickmanTests/stickmanTests.swift
@@ -1,36 +1,57 @@
-//
-//  stickmanTests.swift
-//  stickmanTests
-//
-//  Created by Donald Sheng on 2018-06-24.
-//  Copyright © 2018 GoldThumb Inc. All rights reserved.
-//
-
 import XCTest
 @testable import stickman
 
 class stickmanTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testIfElse03() {
+        let a = 13
+        let b = 11
+        
+        if (a + b) % 2 == 0 {
+            print("even")
+        } else {
+            print("odd")
         }
     }
+    
+    func testIfElse02() {
+        let a = 11
+        let b = 11
+        
+        if a > b {
+            print("a is greater than b")
+        } else {
+            print("a is smaller than b, or a is same as b")
+        }
+    }
+    
+    func testIfElse01() {
+        let a = 12
+    
+        if a % 3 == 0 {
+            print("a is 3的倍数")
+        } else {
+            print("a is not 3的倍数")
+        }
+    }
+    
+    func testLoop() {
+        for i in 1...5 {
+            print("\(i * 1)")
+        }
+    }
+
+    func testExample() {
+        print("My name is Chandler.")
+        
+        let a = 2
+        let b = 3
+        let c = a + b
+        
+        print("a is \(a)")
+        print("b is \(b)")
+        print("c is \(c)")
+    }
+
     
 }
