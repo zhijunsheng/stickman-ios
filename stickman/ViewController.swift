2b181ef9abcaeb9557a5cd9d79210be3881fdac0
Andy Yan <andy@Andys-MacBook-Air.local>
2019-11-22 17:36:25 -0500

    we created a connecttion between stickman view and view controller

--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -4,9 +4,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var stickmanView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(stickmanView.frame.origin.x)
+        
+        stickmanView.frame.origin.x = 700
     }
 
 
