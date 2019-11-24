affeadcb11d4d3f8adc1b23256b27f0125d758af
Andy Yan <andy@Andys-MacBook-Air.local>
2019-11-24 10:47:56 -0500

    we made our first working button in our stickman project

--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -3,18 +3,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     @IBOutlet weak var stickmanView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print(stickmanView.frame.origin.x)
         
-        stickmanView.frame.origin.x = 700
     }
 
+    @IBAction func goLeft(_ sender: UIButton) {
+        // let's say our step length is 50
+        
+        stickmanView.frame.origin.x = stickmanView.frame.origin.x - 50
 
+    }
 }
 
