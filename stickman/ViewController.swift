73b0dade3df40e1e88ebbda398959a415e60bac1
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-11-25 17:00:51 -0500

    I inserted all the buttons into the stickman project and they all work.

--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -4,22 +4,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet weak var stickmanView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(stickmanView.frame.origin.x)
 
-        
-        
     }
-
 
     @IBAction func goRight(_ sender: Any) {
-        stickmanView.frame.origin.x = 10
-        stickmanView.frame.origin.y = 700
+        stickmanView.frame.origin.x = stickmanView.frame.origin.x + 50
+    }
     
+    @IBAction func goUp(_ sender: Any) {
+        stickmanView.frame.origin.y = stickmanView.frame.origin.y - 50
+    }
+   
+    @IBAction func goLeft(_ sender: Any) {
+        stickmanView.frame.origin.x = stickmanView.frame.origin.x - 50
+    }
+    
+    @IBAction func goDown(_ sender: Any) {
+        stickmanView.frame.origin.y = stickmanView.frame.origin.y
+        + 50
+        
     }
 }
-
