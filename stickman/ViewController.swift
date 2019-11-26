448713a05ab5de4a5679ab8b4dabc5f91ff66b3b
Andy Yan <andy@Andys-MacBook-Air.local>
2019-11-26 16:31:11 -0500

    we made four working buttons

--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -23,5 +23,19 @@ class ViewController: UIViewController {
         
         
     }
+    
+    @IBAction func goDown(_ sender: UIButton) {
+        
+        stickmanView.frame.origin.y = stickmanView.frame.origin.y + 50
+        
+        
+        
+        
+    }
+    @IBAction func goRight(_ sender: UIButton) {
+        
+        stickmanView.frame.origin.x = stickmanView.frame.origin.x + 50
+        
+    }
 }
 
