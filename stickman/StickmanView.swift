6fbe23873d3b5cfae1939cde34e820746ad0d684
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-11-27 17:12:14 -0500

    I drew the final figure of the stickman for the snake game!

--- stickman/StickmanView.swift
+++ stickman/StickmanView.swift
@@ -19,14 +19,36 @@ class StickmanView: UIView {
         head.fill()
         
         let body = UIBezierPath()
-        body.move(to: CGPoint(x: 50, y: 50))
-        body.addLine(to: CGPoint(x: 100, y: 100))
+        body.move(to: CGPoint(x: 50, y: 55))
+        body.addLine(to: CGPoint(x: 50, y: 110))
         body.lineWidth = 3
         body.stroke()
         
+        let arm1 = UIBezierPath()
+        arm1.move(to: CGPoint(x: 50, y: 70))
+        arm1.addLine(to: CGPoint(x: 90, y: 110))
+        arm1.lineWidth = 3
+        arm1.stroke()
         
-        // Drawing code
+        let arm2 = UIBezierPath()
+        arm2.move(to: CGPoint(x: 50, y: 70))
+        arm2.addLine(to: CGPoint(x: 20, y: 110))
+        arm2.lineWidth = 3
+        arm2.stroke()
+
+        let leg1 = UIBezierPath()
+        leg1.move(to: CGPoint(x: 50, y: 110))
+        leg1.addLine(to: CGPoint(x: 30, y: 150))
+        leg1.lineWidth = 3
+        leg1.stroke()
+
+        let leg2 = UIBezierPath()
+        leg2.move(to: CGPoint(x: 50, y: 110))
+        leg2.addLine(to: CGPoint(x: 80, y: 150))
+        leg2.lineWidth = 3
+        leg2.stroke()
     }
- 
+    
+    
 
 }
