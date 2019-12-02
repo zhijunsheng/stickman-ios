fa45e16c1732f9f5259328bbb254c35d6b5390ef
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-12-02 16:35:39 -0500

    I added an animation to the stickman in the game.

--- stickman/Stickman2View.swift
+++ stickman/Stickman2View.swift
@@ -1,19 +1,16 @@
 //
-//  StickmanView.swift
+//  Stickman2View.swift
 //  stickman
 //
-//  Created by Angie Yan on 2019-11-25.
+//  Created by Angie Yan on 2019-11-27.
 //  Copyright © 2019 Andy Yan. All rights reserved.
 //
 
 import UIKit
 
-class StickmanView: UIView {
-
+class Stickman2View: UIView {
     
-   
     override func draw(_ rect: CGRect) {
-        
         let head = UIBezierPath(arcCenter: CGPoint(x: 50, y: 50), radius: 10, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
         head.fill()
         
@@ -25,31 +22,46 @@ class StickmanView: UIView {
         
         let arm1 = UIBezierPath()
         arm1.move(to: CGPoint(x: 50, y: 70))
-        arm1.addLine(to: CGPoint(x: 90, y: 110))
+        arm1.addLine(to: CGPoint(x: 70, y: 110))
         arm1.lineWidth = 3
         arm1.stroke()
         
         let arm2 = UIBezierPath()
         arm2.move(to: CGPoint(x: 50, y: 70))
-        arm2.addLine(to: CGPoint(x: 20, y: 110))
+        arm2.addLine(to: CGPoint(x: 40, y: 110))
         arm2.lineWidth = 3
         arm2.stroke()
-
+        
         let leg1 = UIBezierPath()
         leg1.move(to: CGPoint(x: 50, y: 110))
-        leg1.addLine(to: CGPoint(x: 30, y: 150))
+        leg1.addLine(to: CGPoint(x: 10, y: 150))
         leg1.lineWidth = 3
         leg1.stroke()
-
+        
         let leg2 = UIBezierPath()
         leg2.move(to: CGPoint(x: 50, y: 110))
-        leg2.addLine(to: CGPoint(x: 80, y: 150))
+        leg2.addLine(to: CGPoint(x: 100, y: 150))
         leg2.lineWidth = 3
         leg2.stroke()
     }
     
+ 
     
+  
+    
+   
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+  
+ 
 
 }
+
+
+
+
+
 
 
--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -4,7 +4,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var stickmanView: UIView!
+    @IBOutlet weak var stickmanView: StickmanView!
+    
+    @IBOutlet weak var stickman2View: Stickman2View!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,6 +16,13 @@ class ViewController: UIViewController {
 
     @IBAction func goRight(_ sender: Any) {
         stickmanView.frame.origin.x = stickmanView.frame.origin.x + 50
+        stickman2View.frame.origin.x = stickman2View.frame.origin.x + 50
+        
+        if stickmanView.isHidden {
+            stickmanView.isHidden = false
+        } else {
+            stickmanView.isHidden = true
+        }
     }
     
     @IBAction func goUp(_ sender: Any) {
@@ -22,6 +31,13 @@ class ViewController: UIViewController {
    
     @IBAction func goLeft(_ sender: Any) {
         stickmanView.frame.origin.x = stickmanView.frame.origin.x - 50
+         stickman2View.frame.origin.x = stickman2View.frame.origin.x - 50
+        
+        if stickmanView.isHidden {
+            stickmanView.isHidden = false
+        } else {
+            stickmanView.isHidden = true
+        }
     }
     
     @IBAction func goDown(_ sender: Any) {
@@ -29,4 +45,6 @@ class ViewController: UIViewController {
         + 50
         
     }
+    
+    
 }
