7765c5f68479122e2b616dfd6c9793acccbcdfcb
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2019-12-04 22:30:11 +0800

    We have got a button to work.

--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -4,11 +4,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stickmenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func goLeft(_ sender: Any) {
+        print(stickmenView.frame.origin.x)
+       
+       stickmenView.frame.origin.x = stickmenView.frame.origin.x - 30
+        
+    }
 }
 
