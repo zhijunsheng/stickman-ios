e8f231299edcf9dedaf834022a0d65122b63fc30
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-12-07 18:38:20 -0500

    I renamed the first stickmanView to make things consistent in naming.

--- stickman/StickmanView.swift
+++ stickman/StickmanView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class StickmanView: UIView {
+class Stickman1View: UIView {
 
     
    
--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -4,17 +4,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var stickmanView: StickmanView!
+    @IBOutlet weak var stickmanView: Stickman1View!
     
     @IBOutlet weak var stickman2View: Stickman2View!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(stickmanView.frame.origin.x)
+       
+        // if x is greater than 3 we print "hello"; otherwise we print "good morning".
+        let x = 1
+        if x > 3 {
+            print("hello")
+            print("hell2o")
+            print("hello3")
+        } else {
+            print("good morning")
+        }
+        
+        
 
     }
 
     @IBAction func goRight(_ sender: Any) {
+        // stickmanView.frame.origin.x 200
+        // stickmanView.frame.origin.x 250
+        // stickmanView.frame.origin.x 300
+        // stickmanView.frame.origin.x 350
         stickmanView.frame.origin.x = stickmanView.frame.origin.x + 50
         stickman2View.frame.origin.x = stickman2View.frame.origin.x + 50
         
@@ -24,14 +39,19 @@ class ViewController: UIViewController {
             stickmanView.isHidden = true
         }
     }
-    
     @IBAction func goUp(_ sender: Any) {
+//        if stickmanView.frame.origin.y <
+        
         stickmanView.frame.origin.y = stickmanView.frame.origin.y - 50
+        stickman2View.frame.origin.y = stickman2View.frame.origin.y - 50
     }
    
     @IBAction func goLeft(_ sender: Any) {
+        if stickmanView.frame.origin.x < 0.0 {
+            return
+        }
         stickmanView.frame.origin.x = stickmanView.frame.origin.x - 50
-         stickman2View.frame.origin.x = stickman2View.frame.origin.x - 50
+        stickman2View.frame.origin.x = stickman2View.frame.origin.x - 50
         
         if stickmanView.isHidden {
             stickmanView.isHidden = false
@@ -39,12 +59,16 @@ class ViewController: UIViewController {
             stickmanView.isHidden = true
         }
     }
-    
     @IBAction func goDown(_ sender: Any) {
         stickmanView.frame.origin.y = stickmanView.frame.origin.y
         + 50
+        
+        stickman2View.frame.origin.y = stickman2View.frame.origin.y + 50
         
     }
     
     
 }
+
+
+
