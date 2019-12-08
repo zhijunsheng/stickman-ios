51d5f4439e29958eca2c2f801e0f3d1e2f73da32
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2019-12-08 21:08:29 +0800

    We made a stickman on the rectangle.

---
+++ stickman/StickmanView.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class StickmanView: UIView {
+
+    
+    
+    override func draw(_ rect: CGRect) {
+        let pencil = UIBezierPath()
+        
+        pencil.move(to: CGPoint(x: 38, y: 71))
+        pencil.addLine(to: CGPoint(x:77 , y: 121))
+        
+        pencil.move(to: CGPoint(x: 38, y: 71))
+        pencil.addLine(to: CGPoint(x:0 , y: 121))
+        
+        pencil.move(to: CGPoint(x: 38, y: 38))
+        pencil.addLine(to: CGPoint(x:38 , y:123))
+        
+        pencil.move(to: CGPoint(x: 38, y: 123))
+        pencil.addLine(to: CGPoint(x:0 , y:198))
+        
+        pencil.move(to: CGPoint(x: 38, y: 123))
+        pencil.addLine(to: CGPoint(x:77 , y:198))
+        
+        print(bounds)
+        pencil.stroke()
+        
+        let circle = UIBezierPath(arcCenter: CGPoint(x: 38, y: 38), radius:29, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: false)
+        circle.stroke()
+        #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1).setFill()
+        circle.fill()
+    }
+}
