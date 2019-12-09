dd3c30572efe7706d7408930e53ca51f41105c58
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-12-09 16:39:54 -0500

    I fixed the bugs and technical issues in the stickman game.

--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -1,5 +1,3 @@
-
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -10,26 +8,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        // if x is greater than 3 we print "hello"; otherwise we print "good morning".
-        let x = 1
-        if x > 3 {
-            print("hello")
-            print("hell2o")
-            print("hello3")
-        } else {
-            print("good morning")
-        }
-        
-        
-
     }
 
     @IBAction func goRight(_ sender: Any) {
-        // stickmanView.frame.origin.x 200
-        // stickmanView.frame.origin.x 250
-        // stickmanView.frame.origin.x 300
-        // stickmanView.frame.origin.x 350
         stickman1View.frame.origin.x = stickman1View.frame.origin.x + 50
         stickman2View.frame.origin.x = stickman2View.frame.origin.x + 50
         stickman3View.frame.origin.x = stickman3View.frame.origin.x + 50
@@ -40,9 +21,8 @@ class ViewController: UIViewController {
             stickman1View.isHidden = true
         }
     }
+    
     @IBAction func goUp(_ sender: Any) {
-//        if stickmanView.frame.origin.y <
-        
         stickman1View.frame.origin.y = stickman1View.frame.origin.y - 50
         stickman2View.frame.origin.y = stickman2View.frame.origin.y - 50
         stickman3View.frame.origin.y = stickman3View.frame.origin.y - 50
@@ -60,21 +40,18 @@ class ViewController: UIViewController {
         }
         stickman1View.frame.origin.x = stickman1View.frame.origin.x - 50
         stickman2View.frame.origin.x = stickman2View.frame.origin.x - 50
-        
+        stickman3View.frame.origin.x = stickman3View.frame.origin.x - 50
         
         if stickman1View.isHidden {
             stickman1View.isHidden = false
         } else {
             stickman1View.isHidden = true
         }
-        
     }
+    
     @IBAction func goDown(_ sender: Any) {
-        stickman1View.frame.origin.y = stickman1View.frame.origin.y
-        + 50
-        
+        stickman1View.frame.origin.y = stickman1View.frame.origin.y + 50
         stickman2View.frame.origin.y = stickman2View.frame.origin.y + 50
-        
         stickman3View.frame.origin.y = stickman3View.frame.origin.y + 50
         
         if stickman1View.isHidden {
@@ -82,15 +59,5 @@ class ViewController: UIViewController {
         } else {
             stickman1View.isHidden = true
         }
-        
-        
-        
     }
-    
-    
-    
-    
 }
-
-
-
