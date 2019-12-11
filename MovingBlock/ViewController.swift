f176167d944c98c6405844ebf6aba0eb43fc2eb7
Peter Shi <petershi@192.168.0.113>
2019-12-11 11:28:53 +0800

    updated xcode to swift 5

--- MovingBlock/ViewController.swift
+++ MovingBlock/ViewController.swift
@@ -11,46 +11,54 @@ import UIKit
 class ViewController: UIViewController {
     
     let step: CGFloat = 5.0
+//    var look = <#value#>
     
-    @IBOutlet weak var blockView: UIView!
+    @IBOutlet weak var blockView1: StickmanView1!
+    
+    @IBOutlet weak var blockView2: StickmanView2!
+    
+    @IBOutlet weak var blockView3: StickmanView3!
+    
+    @IBOutlet weak var blockView4: StickmanView4!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     @IBAction func leftTapped(_ sender: UIButton) {
-        blockView.frame.origin.x -= step
+        blockView1.frame.origin.x -= step
     }
     
     @IBAction func upTapped(_ sender: UIButton) {
-        blockView.frame.origin.y -= step
+        blockView1.frame.origin.y -= step
     }
     
     @IBAction func downTapped(_ sender: UIButton) {
-        blockView.frame.origin.y += step
+        blockView1.frame.origin.y += step
     }
     
     @IBAction func rightTapped(_ sender: UIButton) {
-        blockView.frame.origin.x += step
+        blockView1.frame.origin.x += step
     }
 
     @IBAction func diagonalTapped1(_ sender: UIButton) {
-        blockView.frame.origin.x -= step
-        blockView.frame.origin.y -= step
+        blockView1.frame.origin.x -= step
+        blockView1.frame.origin.y -= step
     }
     
     @IBAction func diagonalTapped2(_ sender: UIButton) {
-        blockView.frame.origin.x -= step
-        blockView.frame.origin.y += step
+        blockView1.frame.origin.x -= step
+        blockView1.frame.origin.y += step
     }
     
     @IBAction func diagonalTapped3(_ sender: UIButton) {
-        blockView.frame.origin.x += step
-        blockView.frame.origin.y += step
+        blockView1.frame.origin.x += step
+        blockView1.frame.origin.y += step
     }
     
     @IBAction func diagonalTapped4(_ sender: UIButton) {
-        blockView.frame.origin.x += step
-        blockView.frame.origin.y -= step
+        blockView1.frame.origin.x += step
+        blockView1.frame.origin.y -= step
     }
 }
