b0f4bab9298cfe3580c3f27fc492fa48af761030
Elaine <elainexu392@gmail.com>
2019-12-10 19:42:35 -0500

    VimCrypt~03!Y¶ï%fÐV-8$>Ø[@·þ±D¸:êJ{`®\b|¸#sÍáà³Ri9¤ýۧ¦,Þ奘ÂÈ|%ZÛDï&h5ð­Øɝ¤ÎÅÑ+;!\÷Y8o>OUaêc³EùN}ôsâxɢmãі݅çBúÖ{ a¥K®·XòMSñgsW¿zé7V7ÿ7X_üi6 ñÈ>n W1¦.:ð©P%Çá¬7Ê÷7ætJ£ú±b9w¾éÕÙ°JìÞyÒ)wª­6§²¬±(CÎIbºr¬Û.ÖZh?÷?4ÄJ²¨È݄áÒû"MOqmÊEÑƏļ¡wöu?[WzbF­'ÍiԬpm+fºN½@·ãÈÍðH7å@ÖÄ?ñáÒÂsÉÏZz­¦À²±Ê;O²fòfí²±Bz×\T+ÊW·Vö9Ü;U­Å%ξ·¬ºÖàöªâ<Á}Ƃ3ýØ$¦=æS÷]Ü4ÚÀâ(	{ú òxò5,CORF?:U(nßˉ.<lN}ß{Ëtéã !>r«À´Ü2@­õ=³ja

--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -3,28 +3,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var stickmanView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func goLeft(_ sender: Any) {
-          stickmanView.frame.origin.x -= 30
+        if stickmanView.frame.origin.x < 0 {
+            return
+        }
+        stickmanView.frame.origin.x -= 30
     }
     
     @IBAction func goUp(_ sender: Any) {
-           stickmanView.frame.origin.y -= 30
+        stickmanView.frame.origin.y -= 30
     }
     
     @IBAction func goRight(_ sender: Any) {
-           stickmanView.frame.origin.x += 30
-     
+        stickmanView.frame.origin.x += 30
+        
     }
     
     @IBAction func goDown(_ sender: Any) {
-     stickmanView.frame.origin.y += 30
+        stickmanView.frame.origin.y += 30
     }
     
     
