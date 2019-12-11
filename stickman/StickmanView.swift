7fd8c67f1d16fad3aaf531dd811385340d066929
Elaine <elainexu392@gmail.com>
2019-12-10 19:29:15 -0500

    Made the stickman look better and fixed the bug where it would go two directions also gave the stickman a head.:P

--- stickman/StickmanView.swift
+++ stickman/StickmanView.swift
@@ -18,8 +18,13 @@ class StickmanView: UIView {
         pencil.addLine(to: CGPoint(x: 50, y: 100))
         pencil.move(to: CGPoint(x: 150, y: 170))
         pencil.addLine(to: CGPoint(x: 100, y: 150))
-        pencil.move(to: CGPoint(x: 150, y: 160))
+        pencil.move(to: CGPoint(x: 50, y: 170))
         pencil.addLine(to: CGPoint(x: 100, y: 150))
         pencil.stroke()
+        let circle = UIBezierPath(arcCenter: CGPoint(x: 100, y: 50), radius: 21, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        #colorLiteral(red: 0, green: 0.1778146404, blue: 0.2573576627, alpha: 1).setFill()
+        circle.fill()
+        circle.stroke()
+        
     }
 }
