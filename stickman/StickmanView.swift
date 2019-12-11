b2ac925ee87d525142170c9075f259c2d44c0c69
Elaine <elainexu392@gmail.com>
2019-12-10 19:08:19 -0500

    made the view with the stickman and made the view able to move up,left,right and down.

---
+++ stickman/StickmanView.swift
@@ -0,0 +1,25 @@
+//
+//  StickmanView.swift
+//  stickman
+//
+//  Created by Elaine on 2019-12-08.
+//  Copyright © 2019 Andy Yan. All rights reserved.
+//
+
+import UIKit
+
+class StickmanView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+         let pencil = UIBezierPath()
+        pencil.move(to: CGPoint(x: 100, y: 50))
+        pencil.addLine(to: CGPoint(x: 100, y: 150))
+        pencil.move(to: CGPoint(x: 150, y: 100))
+        pencil.addLine(to: CGPoint(x: 50, y: 100))
+        pencil.move(to: CGPoint(x: 150, y: 170))
+        pencil.addLine(to: CGPoint(x: 100, y: 150))
+        pencil.move(to: CGPoint(x: 150, y: 160))
+        pencil.addLine(to: CGPoint(x: 100, y: 150))
+        pencil.stroke()
+    }
+}
--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -4,11 +4,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var stickmanView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func goLeft(_ sender: Any) {
+          stickmanView.frame.origin.x -= 30
+    }
+    
+    @IBAction func goUp(_ sender: Any) {
+           stickmanView.frame.origin.y -= 30
+    }
+    
+    @IBAction func goRight(_ sender: Any) {
+           stickmanView.frame.origin.x += 30
+     
+    }
+    
+    @IBAction func goDown(_ sender: Any) {
+     stickmanView.frame.origin.y += 30
+    }
+    
+    
 }
 
