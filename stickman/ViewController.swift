4fef051d44f8cb8608d5297f0d29090be3fbe657
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2019-12-14 21:29:37 +0800

    We refactored the code by creating a new function animate()

--- stickman/ViewController.swift
+++ stickman/ViewController.swift
@@ -12,47 +12,32 @@ class ViewController: UIViewController {
     @IBAction func goLeft(_ sender: Any) {
         stickman1View.frame.origin.x = stickman1View.frame.origin.x - 30
         stickman2View.frame.origin.x = stickman2View.frame.origin.x - 30
-               
-        if stickman2View.isHidden {
-            stickman2View.isHidden = false
-        } else {
-            stickman2View.isHidden = true
-        }
+        animate()
     }
     
     @IBAction func goRight(_ sender: Any) {
         stickman1View.frame.origin.x = stickman1View.frame.origin.x + 30
         stickman2View.frame.origin.x = stickman2View.frame.origin.x + 30
-        if stickman2View.isHidden {
-            stickman2View.isHidden = false
-        } else {
-            stickman2View.isHidden = true
-        }
+        animate()
     }
     
     @IBAction func goUp(_ sender: Any) {
         stickman1View.frame.origin.y = stickman1View.frame.origin.y - 60
-        
         stickman2View.frame.origin.y = stickman2View.frame.origin.y - 60
-         
-        if stickman2View.isHidden {
-            stickman2View.isHidden = false
-        } else {
-            stickman2View.isHidden = true
-        }
+        animate()
     }
     
     @IBAction func goDown(_ sender: Any) {
         stickman1View.frame.origin.y = stickman1View.frame.origin.y + 60
-        
         stickman2View.frame.origin.y = stickman2View.frame.origin.y + 60
-        
+        animate()
+    }
+    
+    func animate() {
         if stickman2View.isHidden {
             stickman2View.isHidden = false
         } else {
             stickman2View.isHidden = true
         }
-
-    }
-    
+    }    
 }
